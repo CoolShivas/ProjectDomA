48ffe78bee69393f4753b4e58fac000318b45392
CoolShivas <147324057+CoolShivas@users.noreply.github.com>
2023-10-27 12:08:17 +0530

    BlackUndrerLineBorder&FontColor

---
+++ 100--Assignment.js
@@ -0,0 +1,14 @@
+let arr = document.getElementById("header-title");
+//arr.textContent = 'Maxxwell'
+arr.style.borderBottom = 'solid 4px #000';
+
+// let brr = document.getElementsByClassName("title");
+// brr.textContent = 'Product'
+
+// let crr = document.getElementById("main");
+// crr.textContent = 'Produxt'
+
+let drr = document.querySelector('h2');
+drr.style.color = 'green';
+drr.style.fontSize = '50px';
+console.log('hello')
